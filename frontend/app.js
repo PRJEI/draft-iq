64a7031670fc4017e94ddcc99f4a317b67a5a9d8
Eliezel <exi5041@psu.edu>
2026-01-25 00:30:27 -0500

    trying to fix cors

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,12 @@
 const button = document.getElementById("loadBtn");
 const list = document.getElementById("playerList");
 
-const API_BASE = "https://group7project-six.vercel.app/";
 
 button.addEventListener("click", async () => {
   list.innerHTML = "<li>Loading players...</li>";
 
   try {
-    const response = await fetch(`${API_BASE}/api/getPlayers`);
+    const response = await fetch(`https://group7project-six.vercel.app/api/getPlayers`);
     const players = await response.json();
 
     list.innerHTML = "";
