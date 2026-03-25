4990e07bc7d1e5b0433a7e2a62b61ab5a68fd7af
PRJEI <exi5041@psu.edu>
2026-03-25 14:59:01 -0400

    Change Supabase table from 'players' to 'nfl_players'

--- api/getPlayers.js
+++ api/getPlayers.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
   }
 
   const { data, error } = await supabase
-    .from("players")
+    .from("nfl_players")
     .select("*");
 
   if (error) {
