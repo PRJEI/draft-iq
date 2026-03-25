d0c52f32847b11bf1f85cafbdd9a5f2e94e62993
PRJEI <exi5041@psu.edu>
2026-03-25 15:03:10 -0400

    Change player data selection to only 'name'

--- api/getPlayers.js
+++ api/getPlayers.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
   const { data, error } = await supabase
     .from("nfl_players")
-    .select("*");
+    .select("name");
 
   if (error) {
     return res.status(500).json({ error: error.message });
